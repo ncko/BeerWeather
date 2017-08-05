cafec0c2172ee8373a8db25d8256b11ff73ec446
Nick L. Olsen <Nicholas.L.Olsen@gmail.com>
2017-08-05 12:09:09 -0700

    Implement js/progress-screen.js

--- js/progress-screen.js
+++ js/progress-screen.js
@@ -1,7 +1,32 @@
 "use strict";
 
+/*
+ *  This module manages the progress section
+ */
 (function($, eventEmitter) {
 
-  // stub
+  eventEmitter.on('submit-location', showScreen);
+  eventEmitter.on('coordinates-received', coordinatesReceived);
+  eventEmitter.on('forecast-received', forecastReceived);
+  eventEmitter.on('beer-list-received', beerlistReceived);
+
+  const $SCREEN = $('#js-loading-screen');
+  const $PROGRESS_LIST = $('#js-loading-list');
+
+  function showScreen() {
+    $SCREEN.show();
+  }
+
+  function coordinatesReceived(){
+    $PROGRESS_LIST.find('#js-coordinates-received').addClass('done');
+  }
+  
+  function forecastReceived(){
+    $PROGRESS_LIST.find('#js-forecast-received').addClass('done');
+  }
+
+  function beerlistReceived(){
+    $PROGRESS_LIST.find('#js-beer-list-received').addClass('done');
+  }
 
 })(jQuery, window.eventEmitter);
