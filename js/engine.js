9f7db42a80338733c0266e8c31cd3d2449c8ae61
Nick L. Olsen <Nicholas.L.Olsen@gmail.com>
2017-08-08 11:28:38 -0700

    Implement engine.js code

--- js/engine.js
+++ js/engine.js
@@ -1,17 +1,19 @@
 "use strict";
 
-/*
- * The "engine" pulls information from the APIs and generates
- * a beer recommendation
- */
 const ENGINE = (function($, eventEmitter) {
 
   eventEmitter.on('submit-location', fetchCoordinates);
   eventEmitter.on('coordinates-received', fetchForecast);
-  eventEmitter.on('forecast-received', fetchBeer);
-  eventEmitter.on('beer-list-ready', generateBeerRecommendation);
+  eventEmitter.on('forecast-received', fetchBeerStyles);
+  eventEmitter.on('beer-data-ready', generateBeerRecommendations);
+  eventEmitter.on('start-again', init);
 
   const DATA = {};
+
+  function init() {
+    delete DATA.weather;
+    delete DATA.beerStyles;
+  }
 
   function fetchCoordinates( location ) {
     const url = window.encodeURI(`https://geocode.xyz/?scantext=${location}&json=1`);
@@ -20,7 +22,7 @@ const ENGINE = (function($, eventEmitter) {
     });
   }
 
-  function fetchForecast(latitude, longitude){
+  function fetchForecast( latitude, longitude ){
     const url = 'https://api.openweathermap.org/data/2.5/weather';
     const DEFAULT_QUERY = {
       appid: '208b09d63f6454839e7f821fad7776a0',
@@ -29,13 +31,22 @@ const ENGINE = (function($, eventEmitter) {
     };
 
     $.getJSON(url, DEFAULT_QUERY, data => {
-      eventEmitter.emit('forecast-received', data);
       submitData('weather', data);
+      eventEmitter.emit('forecast-received', data);
     });
   }
 
-  function fetchBeer(){
+  function fetchBeerStyles(){
     const beerURL = encodeURIComponent('http://api.brewerydb.com/v2/styles/?key=8a9ed8ea98f0e79ee32f70659adfd782');
+    const jsonpURL = `https://json2jsonp.com/?url=${beerURL}&callback=ENGINE.beerStylesCB`;
+
+    var tag = document.createElement("script");
+    tag.src = jsonpURL;
+    document.getElementsByTagName("head")[0].appendChild(tag);
+  }
+
+  function fetchBeersByStyleID( id, fn ) {
+    const beerURL = encodeURIComponent(`http://api.brewerydb.com/v2/beers/?key=8a9ed8ea98f0e79ee32f70659adfd782&styleId=${id}`);
     const jsonpURL = `https://json2jsonp.com/?url=${beerURL}&callback=ENGINE.beerCB`;
 
     var tag = document.createElement("script");
@@ -43,47 +54,35 @@ const ENGINE = (function($, eventEmitter) {
     document.getElementsByTagName("head")[0].appendChild(tag);
   }
 
-  function generateBeerRecommendation(weather, beer){
+  function generateBeerRecommendations(weather, beer){
+    let style = beer.data[ Math.floor(Math.random() * beer.data.length) ];
 
-    console.log(weather);
-    console.log(beer);
 
-    let beerList = [
-      {
-        title: 'Yum',
-        description: 'It is yummy.'
-      },
-      {
-        title: 'Delish',
-        description: 'Can\'t beat it.'
-      },
-      {
-        title: 'Meh',
-        description: 'It is beatable.'
-      },
-      {
-        title: 'Fake Beer',
-        description: 'Rich, decadent and full-bodied. The intense, roasted malt flavor is derived from two types of chocolate malt and raw cocoa nibs. Whole vanilla beans introduced post-fermentation adds complexity and enhances the desert-like quality.'
-      }
-    ];
+    // Uses JSONP to call FETCHER.beerCB()
+    // FETCHER.beerCB emits 'beer-list-received'
+    fetchBeersByStyleID( style.id );
+    eventEmitter.on('beer-list-received', data => {
+      eventEmitter.emit('beer-recommendations-ready', data.data );
+    } );
 
-    eventEmitter.emit('beer-recommendations-ready', beerList);
   }
 
-  function submitData( type, data ) {
-    if (type === 'weather' || type ==='beer') {
-      DATA[type] = data;
+  function submitData(type, data) {
+    if (type === 'weather' || type === 'beerStyles') {
+      DATA[type]=data;
     }
 
-    if (DATA.weather && DATA.beer) {
-      eventEmitter.emit('beer-list-ready', DATA.weather, DATA.beer);
-    }
+    if (DATA['weather'] && DATA['beerStyles'])
+      eventEmitter.emit('beer-data-ready', DATA.weather, DATA.beerStyles);
   }
 
   return {
+    beerStylesCB: function( data ) {
+      submitData('beerStyles', data);
+      eventEmitter.emit('beer-styles-received');
+    },
     beerCB: function( data ) {
-      eventEmitter.emit('beer-list-received');
-      submitData('beer', data);
+      eventEmitter.emit('beer-list-received', data);
     }
   }
 
